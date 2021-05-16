410aa5ee9c083a4b667f70511cf16bb40be4d1a0
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-05-16 15:14:21 -0300

    Main options updated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #==========[Local Libraries]==========
 #-----[Functionality]-----
 from src.helper.img import get_pixels
+from src.helper.matrix import print_matrix
+from src.h_equalization.equalization import *
 
 #-----[Terminal Interface]-----
 import src.cli.menu as menu
@@ -9,6 +11,7 @@ import src.cli.menu as menu
 def main():
   img_original = menu.load()
   img_matrix = get_pixels(img_original)
+  img_columns, img_lines = img_original.size
 
   while True:
     selection = menu.home()
@@ -20,6 +23,17 @@ def main():
       img_matrix = get_pixels(img_original)
 
     elif selection == 2:
+      gray_scale_img = to_gray_scale(img_original, img_lines, img_columns)
+      gray_img_matrix = get_pixels( gray_scale_img, img_lines, img_columns )
+      
+      nk = get_histogram(gray_img_matrix, img_lines, img_columns)
+      pr_rk = get_pr_rk(nk)
+      frequencia = get_freq(pr_rk)
+      eq = get_eq(frequencia)
+      new_rk = get_new_rk(eq)
+      new_img = equalize(gray_img_matrix, new_rk, img_lines, img_columns)
+
+    elif selection == 3:
       menu.export(img_matrix)
 
 #==========[Script Initializer]==========
