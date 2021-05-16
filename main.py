0b5c134574b061db6b4bd7e18738f9b39bdd6dc2
h80r <h80rgbc@gmail.com>
2021-05-16 19:10:42 -0300

    Main menu restored

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 #==========[Local Libraries]==========
 #-----[Functionality]-----
-from src.histogram.histogram import Histogram
-from src.helper.img import get_pixels, import_image
+from src.helper.img import get_pixels
 
-#-----[Terminal]-----
-from src.helper.histogram import print_basic
+#-----[Terminal Interface]-----
+import src.cli.menu as menu
 
 #==========[Main Function]==========
-
 def main():
-  img_original = import_image('./sample/color_test.png')
+  img_original = menu.load()
   img_matrix = get_pixels(img_original)
 
-  test = Histogram(img_matrix)
-  print_basic(test.basic(img_matrix[0][0]))
+  while True:
+    selection = menu.home()
 
+    if selection == 0:
+      break
+
+    elif selection == 1:
+      img_matrix = get_pixels(img_original)
+
+    elif selection == 2:
+      menu.export(img_matrix)
 
 #==========[Script Initializer]==========
 if __name__ == "__main__":
